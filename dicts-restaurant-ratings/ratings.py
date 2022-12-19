affcb7fc76487769a42114870b52d6cdaed9f758
bethany <bethanylynnpro@gmail.com>
2022-12-19 12:21:01 -0800

    lab

--- dicts-restaurant-ratings/ratings.py
+++ dicts-restaurant-ratings/ratings.py
@@ -1,4 +1,13 @@
 """Restaurant rating lister."""
+# use .items() to get list from dictionary
+# sorted() will return sorted information, but wont save sorted inside original dictionary
 
 
-# put your code here
+
+score = {}
+file = open("scores.txt")
+
+#define function here
+
+
+#return dictionary
